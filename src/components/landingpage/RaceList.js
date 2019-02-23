71a8c0d6b7779aecda99946c20b1ce756c02aa6c
DESKTOP-GE1MGRF\box <davidlynch2000@yahoo.com>
2019-02-23 17:01:00 -0600

    skeleton for race summary

--- src/components/landingpage/RaceList.js
+++ src/components/landingpage/RaceList.js
@@ -1,30 +1,15 @@
 import React from 'react';
+import RaceSummary from '../races/RaceSummary';
 
 const RaceList = () =>{
 
     let date = new Date();
     return(
         <div className='section'>
-            <div className='card'>
-                <span className='card-title'>Captex 2014</span>
-                <p> I'm slow, but also, kiiiiinda awkward</p>
-                <p> Tee-Ran-A-Sore-ous Wrecks </p>
-                <p className='grey-text'>{date.toLocaleDateString()}</p>
-            </div>
-
-            <div className='card'>
-                <span className='card-title'> Mean Green</span>
-                <p> Ugh... self-seeded swims</p>
-                <p> Tee-Ran-A-Sore-ous Wrecks </p>
-                <p className='grey-text'>{date.toLocaleDateString()}</p>
-            </div>
-
-            <div className='card'>
-                <span className='card-title'> Cap 10k</span>
-                <p> They're all getting away!</p>
-                <p> Tee-Ran-A-Sore-ous Wrecks </p>
-                <p className='grey-text'>{date.toLocaleDateString()}</p>
-            </div>
+            <RaceSummary date={date.toLocaleDateString()}/>
+            <RaceSummary date={date.toLocaleDateString()}/>
+            <RaceSummary date={date.toLocaleDateString()}/>
+            <RaceSummary date={date.toLocaleDateString()}/>
         </div>
     );
 };
