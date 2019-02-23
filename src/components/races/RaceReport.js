b134d58744cdd90c07dc9d8014f764c4fd197aad
DESKTOP-GE1MGRF\box <davidlynch2000@yahoo.com>
2019-02-23 17:25:25 -0600

    missed added file

---
+++ src/components/races/RaceReport.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const RaceReport = (props) =>{
+    return(
+        <div className='container section'>
+            <div className='card'>
+            <span className='card-title'>Dummy</span>
+            <p>AVERT THINE EYES, LEST I UNINTENTIONALLY RUIN YOUR FINISHING PHOTO</p>
+            <p>D-Ran-A-Sore Wrecks</p>
+            <p>date</p>
+            </div>
+        </div>
+    );
+}
+
+export default RaceReport;
