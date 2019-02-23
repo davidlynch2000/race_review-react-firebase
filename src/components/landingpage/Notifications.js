fd2cafacd38c5d24ddafacf418477a2f6872bf18
DESKTOP-GE1MGRF\box <davidlynch2000@yahoo.com>
2019-02-23 01:18:14 -0600

    Basic landing page skeleton

---
+++ src/components/landingpage/Notifications.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Notifications = () =>{
+    return(
+        <div className='section'>
+            <p>
+                Another
+            </p>
+            <p>
+                And Another
+            </p>
+            <p>
+                And Another
+            </p>
+        </div>
+    );
+};
+
+export default Notifications;
