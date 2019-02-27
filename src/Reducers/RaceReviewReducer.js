582dbc79871ca1adc4272ed3cdc52ddab7e33c1a
DESKTOP-GE1MGRF\box <davidlynch2000@yahoo.com>
2019-02-27 01:34:14 -0600

    no more dummy data

--- src/Reducers/RaceReviewReducer.js
+++ src/Reducers/RaceReviewReducer.js
@@ -1,10 +1,4 @@
-const initState = {
-    raceReviews:[
-        {id:1,title:'Captex 2014', summary:'Keith Got Run Over By A MotorCycle',author:'D-Ran_Is-Sore(ish)/Wrecked', created:new Date()},
-        {id:2,title:'Cap 10K 2018', summary:"I'm a big believer in... evaporative cooling",author:'D-Ran_Is-Sore(ish)/Wrecked', created:new Date()},
-        {id:3,title:'Trifecta 2014', summary:"Which way should we go?",author:'D-Ran_Is-Sore(ish)/Wrecked', created:new Date()},
-    ],
-};
+const initState = {};
 
 const raceReviewReducer = (state = initState,action) =>{
     switch(action.type){
