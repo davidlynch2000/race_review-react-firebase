2512c86720cf5091c79236221e80a8fd75a3dedf
DESKTOP-GE1MGRF\box <davidlynch2000@yahoo.com>
2019-02-27 21:05:25 -0600

    allows you to click into the individual race reviews that are on the landing page

--- src/components/races/RaceSummary.js
+++ src/components/races/RaceSummary.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './RaceSummary.css';
 
+
 const RaceSummary = ({review}) =>{
     return(
-        <div className='card'>
+
+        <div className='card' >
             <span className='card-title'>{review.title}</span>
             <p>{review.summary}</p>
             <p>{review.author}</p>
             <p>{review.created.toLocaleDateString()}</p>
         </div>
+
     );
 }
 
