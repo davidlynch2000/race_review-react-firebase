10b15d48b24b6b857da8e55b5724bdaaa398bdab
DESKTOP-GE1MGRF\box <davidlynch2000@yahoo.com>
2019-03-19 12:11:56 -0500

    crude skeleton

--- src/components/authentication/Settings.js
+++ src/components/authentication/Settings.js
@@ -1,15 +1,46 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {connect} from 'react-redux';
 
-class Settings extends Component{
-    render(){
-        return(
-            <div className='container section'>
-                <form>
-                    <h1>Settings</h1>
-                </form>
+const Settings = (props) => {
+    
+    return(
+        <div className='container section'>
+            <div className='card'>
+                <div className='title'>
+                    <span className='card-title'>Settings</span>
+                </div>
+                <div>
+                    <label> Name</label>
+                    <span>{'First Name'}</span><i className="material-icons">edit</i>
+                    <span>{'Last Name'}</span><i className="material-icons">edit</i>
+                </div>
+                <div>
+                    <label> Username </label>
+                    <span>{'Username'}</span><i className="material-icons">edit</i>
+                </div>
+                {/* {editable ? 
+                    (
+                    <div className='row buttonRow'>
+                        <Link to={'/editracereport/'+props.match.params.id}>
+                            <button 
+                                className='teal lighten-4 input-field col l2 m2 s4 offset-l3 offset-m3'>
+                                    Edit
+                            </button>
+                        </Link>
+                        <button
+                            className='teal lighten-4 input-field col l2 m2 s4 offset-l2 offset-m2'>
+                                <DeletionModal id={props.match.params.id} history={props.history} />
+                        </button>
+                    </div>
+                    )
+                    :
+                    ('')
+                } */}
+            
             </div>
-        );
-    }
+        </div>
+    );
+
 }
 
-export default Settings;
+export default connect()(Settings);
